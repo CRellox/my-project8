9469e5bf3ccf95b13e0e97ad168de2c07481d2be
Chris Ladas <chrisladas98@gmail.com>
2025-06-04 23:09:25 +0300

    fix: add print formatted by main branch

--- src/UNO/Deck.java
+++ src/UNO/Deck.java
@@ -2,73 +2,125 @@ package UNO;
 
 public class Deck {
 
-        // Red Cards
-        Cards card0R = new Cards("Red","0");
-        Cards card1R = new Cards("Red","1");
-        Cards card2R = new Cards("Red","2");
-        Cards card3R = new Cards("Red","3");
-        Cards card4R = new Cards("Red","4");
-        Cards card5R = new Cards("Red","5");
-        Cards card6R = new Cards("Red","6");
-        Cards card7R = new Cards("Red","7");
-        Cards card8R = new Cards("Red","8");
-        Cards card9R = new Cards("Red","9");
-        Cards cardRR = new Cards("Red","Reverse");
-        Cards cardSR = new Cards("Red","Skip");
-        Cards cardP2R = new Cards("Red","Plus2");
+    // Red Standard Cards
+    Cards card1R1 = new Cards("Red","1");
+    Cards card1R2 = new Cards("Red","1");
+    Cards card2R1 = new Cards("Red","2");
+    Cards card2R2 = new Cards("Red","2");
+    Cards card3R1 = new Cards("Red","3");
+    Cards card3R2 = new Cards("Red","3");
+    Cards card4R1 = new Cards("Red","4");
+    Cards card4R2 = new Cards("Red","4");
+    Cards card5R1 = new Cards("Red","5");
+    Cards card5R2 = new Cards("Red","5");
+    Cards card6R1 = new Cards("Red","6");
+    Cards card6R2 = new Cards("Red","6");
+    Cards card7R1 = new Cards("Red","7");
+    Cards card7R2 = new Cards("Red","7");
+    Cards card8R1 = new Cards("Red","8");
+    Cards card8R2 = new Cards("Red","8");
+    Cards card9R1 = new Cards("Red","9");
+    Cards card9R2 = new Cards("Red","9");
 
-        // Blue Cards
-        Cards card0B = new Cards("Blue","0");
-        Cards card1B = new Cards("Blue","1");
-        Cards card2B = new Cards("Blue","2");
-        Cards card3B = new Cards("Blue","3");
-        Cards card4B = new Cards("Blue","4");
-        Cards card5B = new Cards("Blue","5");
-        Cards card6B = new Cards("Blue","6");
-        Cards card7B = new Cards("Blue","7");
-        Cards card8B = new Cards("Blue","8");
-        Cards card9B = new Cards("Blue","9");
-        Cards cardRB = new Cards("Blue","Reverse");
-        Cards cardSB = new Cards("Blue","Skip");
-        Cards cardP2B = new Cards("Blue","Plus2");
+    // Red Special Cards
+    SpecialCards cardRR1 = new SpecialCards("Red","Reverse");
+    SpecialCards cardRR2 = new SpecialCards("Red","Reverse");
+    SpecialCards cardSR1 = new SpecialCards("Red","Skip");
+    SpecialCards cardSR2 = new SpecialCards("Red","Skip");
+    SpecialCards cardP2R1 = new SpecialCards("Red","Plus2");
+    SpecialCards cardP2R2 = new SpecialCards("Red","Plus2");
 
-        // Green Cards
-        Cards card0G = new Cards("Green","0");
-        Cards card1G = new Cards("Green","1");
-        Cards card2G = new Cards("Green","2");
-        Cards card3G = new Cards("Green","3");
-        Cards card4G = new Cards("Green","4");
-        Cards card5G = new Cards("Green","5");
-        Cards card6G = new Cards("Green","6");
-        Cards card7G = new Cards("Green","7");
-        Cards card8G = new Cards("Green","8");
-        Cards card9G = new Cards("Green","9");
-        Cards cardRG = new Cards("Green","Reverse");
-        Cards cardSG = new Cards("Green","Skip");
-        Cards cardP2G = new Cards("Green","Plus2");
+    // Blue Standard Cards
+    Cards card1B1 = new Cards("Blue","1");
+    Cards card1B2 = new Cards("Blue","1");
+    Cards card2B1 = new Cards("Blue","2");
+    Cards card2B2 = new Cards("Blue","2");
+    Cards card3B1 = new Cards("Blue","3");
+    Cards card3B2 = new Cards("Blue","3");
+    Cards card4B1 = new Cards("Blue","4");
+    Cards card4B2 = new Cards("Blue","4");
+    Cards card5B1 = new Cards("Blue","5");
+    Cards card5B2 = new Cards("Blue","5");
+    Cards card6B1 = new Cards("Blue","6");
+    Cards card6B2 = new Cards("Blue","6");
+    Cards card7B1 = new Cards("Blue","7");
+    Cards card7B2 = new Cards("Blue","7");
+    Cards card8B1 = new Cards("Blue","8");
+    Cards card8B2 = new Cards("Blue","8");
+    Cards card9B1 = new Cards("Blue","9");
+    Cards card9B2 = new Cards("Blue","9");
 
-        // Yellow Cards
-        Cards card0Y = new Cards("Yellow","0");
-        Cards card1Y = new Cards("Yellow","1");
-        Cards card2Y = new Cards("Yellow","2");
-        Cards card3Y = new Cards("Yellow","3");
-        Cards card4Y = new Cards("Yellow","4");
-        Cards card5Y = new Cards("Yellow","5");
-        Cards card6Y = new Cards("Yellow","6");
-        Cards card7Y = new Cards("Yellow","7");
-        Cards card8Y = new Cards("Yellow","8");
-        Cards card9Y = new Cards("Yellow","9");
-        Cards cardRY = new Cards("Yellow","Reverse");
-        Cards cardSY = new Cards("Yellow","Skip");
-        Cards cardP2Y = new Cards("Yellow","Plus2");
+    // Blue Special Cards
+    SpecialCards cardRB1 = new SpecialCards("Blue","Reverse");
+    SpecialCards cardRB2 = new SpecialCards("Blue","Reverse");
+    SpecialCards cardSB1 = new SpecialCards("Blue","Skip");
+    SpecialCards cardSB2 = new SpecialCards("Blue","Skip");
+    SpecialCards cardP2B1 = new SpecialCards("Blue","Plus2");
+    SpecialCards cardP2B2 = new SpecialCards("Blue","Plus2");
 
-        // Wild Cards
-        WildCards Wild1 = new WildCards("Wild");
-        WildCards Wild2 = new WildCards("Wild");
-        WildCards Wild3 = new WildCards("Wild");
-        WildCards Wild4 = new WildCards("Wild");
-        WildCards DrawFourCards1 = new WildCards("Wild Draw Four");
-        WildCards DrawFourCards2 = new WildCards("Wild Draw Four");
-        WildCards DrawFourCards3 = new WildCards("Wild Draw Four");
-        WildCards DrawFourCards4 = new WildCards("Wild Draw Four");
+    // Green Standard Cards
+    Cards card1G1 = new Cards("Green","1");
+    Cards card1G2 = new Cards("Green","1");
+    Cards card2G1 = new Cards("Green","2");
+    Cards card2G2 = new Cards("Green","2");
+    Cards card3G1 = new Cards("Green","3");
+    Cards card3G2 = new Cards("Green","3");
+    Cards card4G1 = new Cards("Green","4");
+    Cards card4G2 = new Cards("Green","4");
+    Cards card5G1 = new Cards("Green","5");
+    Cards card5G2 = new Cards("Green","5");
+    Cards card6G1 = new Cards("Green","6");
+    Cards card6G2 = new Cards("Green","6");
+    Cards card7G1 = new Cards("Green","7");
+    Cards card7G2 = new Cards("Green","7");
+    Cards card8G1 = new Cards("Green","8");
+    Cards card8G2 = new Cards("Green","8");
+    Cards card9G1 = new Cards("Green","9");
+    Cards card9G2 = new Cards("Green","9");
+
+    // Green Special Cards
+    SpecialCards cardRG1 = new SpecialCards("Green","Reverse");
+    SpecialCards cardRG2 = new SpecialCards("Green","Reverse");
+    SpecialCards cardSG1 = new SpecialCards("Green","Skip");
+    SpecialCards cardSG2 = new SpecialCards("Green","Skip");
+    SpecialCards cardP2G1 = new SpecialCards("Green","Plus2");
+    SpecialCards cardP2G2 = new SpecialCards("Green","Plus2");
+
+    // Yellow Standard Cards
+    Cards card1Y1 = new Cards("Yellow","1");
+    Cards card1Y2 = new Cards("Yellow","1");
+    Cards card2Y1 = new Cards("Yellow","2");
+    Cards card2Y2 = new Cards("Yellow","2");
+    Cards card3Y1 = new Cards("Yellow","3");
+    Cards card3Y2 = new Cards("Yellow","3");
+    Cards card4Y1 = new Cards("Yellow","4");
+    Cards card4Y2 = new Cards("Yellow","4");
+    Cards card5Y1 = new Cards("Yellow","5");
+    Cards card5Y2 = new Cards("Yellow","5");
+    Cards card6Y1 = new Cards("Yellow","6");
+    Cards card6Y2 = new Cards("Yellow","6");
+    Cards card7Y1 = new Cards("Yellow","7");
+    Cards card7Y2 = new Cards("Yellow","7");
+    Cards card8Y1 = new Cards("Yellow","8");
+    Cards card8Y2 = new Cards("Yellow","8");
+    Cards card9Y1 = new Cards("Yellow","9");
+    Cards card9Y2 = new Cards("Yellow","9");
+
+    // Yellow Special Cards
+    SpecialCards cardRY1 = new SpecialCards("Yellow","Reverse");
+    SpecialCards cardRY2 = new SpecialCards("Yellow","Reverse");
+    SpecialCards cardSY1 = new SpecialCards("Yellow","Skip");
+    SpecialCards cardSY2 = new SpecialCards("Yellow","Skip");
+    SpecialCards cardP2Y1 = new SpecialCards("Yellow","Plus2");
+    SpecialCards cardP2Y2 = new SpecialCards("Yellow","Plus2");
+
+    // Wild Cards
+    WildCards Wild1 = new WildCards("Wild");
+    WildCards Wild2 = new WildCards("Wild");
+    WildCards Wild3 = new WildCards("Wild");
+    WildCards Wild4 = new WildCards("Wild");
+    WildCards DrawFourCards1 = new WildCards("Wild Draw Four");
+    WildCards DrawFourCards2 = new WildCards("Wild Draw Four");
+    WildCards DrawFourCards3 = new WildCards("Wild Draw Four");
+    WildCards DrawFourCards4 = new WildCards("Wild Draw Four");
 }
