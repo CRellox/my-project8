09469458d5d2852acaecb3362b311b55c18080ba
Chris <chrisladas98@gmail.com>
2025-06-12 00:36:14 +0300

    package with exceptions added.

---
+++ src/gr/ladas/uno/v1/exceptions/WrongColoredCardException.java
@@ -0,0 +1,8 @@
+package gr.ladas.uno.v1.exceptions;
+
+public class WrongColoredCardException extends Exception {
+
+    public WrongColoredCardException(String message) {
+        super(message);
+    }
+}
---
+++ src/gr/ladas/uno/v1/exceptions/WrongNumberedCardException.java
@@ -0,0 +1,8 @@
+package gr.ladas.uno.v1.exceptions;
+
+public class WrongNumberedCardException extends Exception {
+
+    public WrongNumberedCardException(String message) {
+        super(message);
+    }
+}
